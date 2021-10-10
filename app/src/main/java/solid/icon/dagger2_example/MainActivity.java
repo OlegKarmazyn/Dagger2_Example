09378fcc55541cc90345bc4dbfdaf81ef90cba80
UnameStar1 <ol0321k@gmail.com>
2021-10-10 16:52:28 +0300

    inject networkService to MainActivity.java

--- app/src/main/java/solid/icon/dagger2_example/MainActivity.java
+++ app/src/main/java/solid/icon/dagger2_example/MainActivity.java
@@ -1,14 +1,29 @@
 package solid.icon.dagger2_example;
 
+import android.os.Bundle;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import javax.inject.Inject;
+
+import solid.icon.dagger2_example.services.NetworkService;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView textView;
+
+    @Inject
+    NetworkService networkService;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ((App)getApplication()).getAppComponent()
+                .inject(this);
+
+        textView = findViewById(R.id.textView);
+        textView.setText(networkService.getHello());
     }
 }
