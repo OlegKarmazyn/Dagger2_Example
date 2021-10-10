1f1e5b35467d1c9e7154384d6c6f1008cb912c02
UnameStar1 <ol0321k@gmail.com>
2021-10-10 15:51:11 +0300

    added App.class with "extends Application"

---
+++ app/src/main/java/solid/icon/dagger2_example/App.java
@@ -0,0 +1,11 @@
+package solid.icon.dagger2_example;
+
+import android.app.Application;
+
+public class App extends Application{
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
