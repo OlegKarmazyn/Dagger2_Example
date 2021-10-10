0c0571d0d49de6ff0d740420555ef986a04896b8
UnameStar1 <ol0321k@gmail.com>
2021-10-10 16:46:29 +0300

    added init appComponent to App.class

--- app/src/main/java/solid/icon/dagger2_example/App.java
+++ app/src/main/java/solid/icon/dagger2_example/App.java
@@ -2,10 +2,23 @@ package solid.icon.dagger2_example;
 
 import android.app.Application;
 
+import solid.icon.dagger2_example.di.component.AppComponent;
+import solid.icon.dagger2_example.di.component.DaggerAppComponent;
+import solid.icon.dagger2_example.di.modules.AppModule;
+
 public class App extends Application{
+
+    private AppComponent appComponent;
 
     @Override
     public void onCreate() {
         super.onCreate();
+        appComponent = DaggerAppComponent.builder()
+                .appModule(new AppModule(this))
+                .build();
+    }
+
+    public AppComponent getAppComponent() {
+        return appComponent;
     }
 }
